cadf149880ffc16ed3d4ab51abb2c5ed75dc34f7
goncharov-roman <romg92@mail.ru>
2018-07-02 17:51:45 +0300

    django added

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.py /etc/gunicorn.d/test
-sudo gunicorn -c /etc/gunicorn.d/test hello:app
+#sudo ln -sf /home/box/web/etc/gunicorn.py /etc/gunicorn.d/test
+#sudo gunicorn -c /etc/gunicorn.d/test hello:app
+sudo gunicorn -c /home/box/web/etc/gunicorn.py hello:app
+sudo gunicorn -c /home/box/web/etc/gunicorn-django.py ask.wsgi:application
